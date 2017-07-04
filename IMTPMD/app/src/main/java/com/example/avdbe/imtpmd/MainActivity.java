4640d71145a83d1f39fcbc51f8620f51122c5487
Mathijs1995 <mathijs1995@live.nl>
2017-07-04 15:39:23 +0200

    Navigatie menu werkend

--- IMTPMD/app/src/main/java/com/example/avdbe/imtpmd/MainActivity.java
+++ IMTPMD/app/src/main/java/com/example/avdbe/imtpmd/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.avdbe.imtpmd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public void init() {
+
+
+        final Button bHome = (Button) findViewById(R.id.bHome);
+        bHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent1 = new Intent(MainActivity.this, NavActivity.class);
+                startActivity(intent1);
+            }
+
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+
+
+        }
     }
-}
